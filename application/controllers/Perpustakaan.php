c81796aa5073e7f4920564a41a79e1ee113b140a
AaEzha <aaezha@gmail.com>
2021-08-16 14:20:14 +0700

    lesson-9

--- application/controllers/Perpustakaan.php
+++ application/controllers/Perpustakaan.php
@@ -61,8 +61,8 @@ class Perpustakaan extends CI_Controller
 		$crud->unset_columns(['password', 'created_at', 'updated_at']);
 		// $crud->unset_edit_fields(['password']);
 		$crud->callback_edit_field('password', function ($value, $primary_key) {
-            return '<input type="password" maxlength="50" value="" name="password">';
-        });
+			return '<input type="password" maxlength="50" value="" name="password">';
+		});
 		$crud->callback_before_update(array($this, 'update_password_callback'));
 		$crud->unset_fields(['created_at', 'updated_at']);
 
@@ -77,18 +77,16 @@ class Perpustakaan extends CI_Controller
 		return $post_array;
 	}
 
-	function update_password_callback($post_array, $primary_key) {
+	function update_password_callback($post_array, $primary_key)
+	{
 		//Encrypt password only if is not empty. Else don't change the password to an empty field
-		if(!empty($post_array['password']))
-		{
+		if (!empty($post_array['password'])) {
 			$post_array['password'] = md5($post_array['password']);
-		}
-		else
-		{
+		} else {
 			unset($post_array['password']);
 		}
 
-	  return $post_array;
+		return $post_array;
 	}
 
 	public function provinsi()
@@ -98,7 +96,7 @@ class Perpustakaan extends CI_Controller
 
 		$crud->set_subject('Provinsi', 'Daftar Provinsi');
 		$crud->set_theme('datatables');
-		$crud->callback_column('provinsi',array($this,'_callback_webpage_provinsi'));
+		$crud->callback_column('provinsi', array($this, '_callback_webpage_provinsi'));
 
 		$output = $crud->render();
 
@@ -106,7 +104,32 @@ class Perpustakaan extends CI_Controller
 	}
 
 	public function _callback_webpage_provinsi($value, $row)
-{
-  return "<a href='".site_url('perpustakaan/kabupaten/'.$row->id)."'>$value</a>";
-}
+	{
+		return "<a href='" . site_url('perpustakaan/kabupaten/' . $row->id) . "'>$value</a>";
+	}
+
+	public function kabupaten($provinsi_id)
+	{
+		$crud = new grocery_CRUD();
+		$crud->set_table('kabupaten');
+
+		$crud->set_subject('Kabupaten', 'Daftar Kabupaten');
+		$crud->set_theme('datatables');
+		$crud->callback_column('kabupaten', function($value, $row){
+			return "<a href='" . site_url('perpustakaan/kecamatan/' . $row->id) . "'>$value</a>";
+		});
+		$crud->where('provinsi_id', $provinsi_id);
+		$crud->display_as('provinsi_id', 'Provinsi');
+		$crud->fields('kabupaten', 'provinsi_id');
+		$crud->field_type('provinsi_id', 'hidden', $provinsi_id);
+		// $crud->set_relation('provinsi_id', 'provinsi', 'provinsi');
+		$crud->callback_column('provinsi_id', function ($value, $row) {
+			$res = $this->db->get_where('provinsi', ['id' => $value])->row();
+			return $res->provinsi;
+		});
+
+		$output = $crud->render();
+
+		$this->_example_output($output);
+	}
 }
